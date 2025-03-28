c8ac98d52c3617e58dfa961169feadcadaef9977
ivanjim13 <ivangorrion57@gmail.com>
2025-03-28 13:33:51 +0100

    login creado y navegacion implementada

--- composeApp/src/commonMain/kotlin/org/example/project/widgetsScreen/Calendar.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/widgetsScreen/Calendar.kt
@@ -148,81 +148,81 @@ fun diaActual(): LocalDate {
     return Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
 }
 
-@Composable
-fun PantallaCalendario() {
-    MaterialTheme {
-        Scaffold(bottomBar = {
-            BottomNavigationBar()
-        }) {
-            Column {
-                Calendar()
-                XYSamplePlot()
-            }
-        }
-    }
-}
-
-@Composable
-fun BottomNavigationBar() {
-    var selected by remember { mutableStateOf(0) }
-    val items = listOf("Home", "Calendar", "Notifications", "Profile")
-    val icons = listOf(
-        Icons.Filled.Home,
-        Icons.Filled.DateRange,
-        Icons.Filled.Notifications,
-        Icons.Filled.Person
-    )
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(80.dp)
-            .background(Color.White)
-            .graphicsLayer { shadowElevation = 10f }
-    ) {
-        FloatingActionButton(onClick = {},
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .offset(y = (-30).dp), // Posición del FAB sobre el hueco
-            shape = CircleShape,
-            contentColor = Color(0xFF7F57FF)){
-            Icon(Icons.Filled.Add, contentDescription = "Menu")
-        }
-        Canvas(modifier = Modifier.matchParentSize()) {
-            val width = size.width
-            val height = size.height
-            val curveSize = 100f
-            val curveHeight = 140f
-
-            val path = Path().apply {
-                moveTo(0f, 0f)
-                lineTo((width / 2) - curveSize, 0f)
-                quadraticBezierTo(width / 2, curveHeight, (width / 2) + curveSize, 0f)
-                lineTo(width, 0f)
-                lineTo(width, height)
-                lineTo(0f, height)
-                close()
-            }
-            drawPath(path, color = Color.White)
-        }
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp, vertical = 12.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            items.forEachIndexed { index, item ->
-                IconButton(onClick = { selected = index }) {
-                    Icon(
-                        imageVector = icons[index],
-                        contentDescription = item,
-                        tint = if (selected == index) Color(0xFF7F57FF) else Color.Gray,
-                        modifier = Modifier.size(28.dp)
-                    )
-                }
-            }
-        }
-    }
-}
+//@Composable
+//fun PantallaCalendario() {
+//    MaterialTheme {
+//        Scaffold(bottomBar = {
+//            BottomNavigationBar()
+//        }) {
+//            Column {
+//                Calendar()
+//                XYSamplePlot()
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun BottomNavigationBar() {
+//    var selected by remember { mutableStateOf(0) }
+//    val items = listOf("Home", "Calendar", "Notifications", "Profile")
+//    val icons = listOf(
+//        Icons.Filled.Home,
+//        Icons.Filled.DateRange,
+//        Icons.Filled.Notifications,
+//        Icons.Filled.Person
+//    )
+//
+//    Box(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .height(80.dp)
+//            .background(Color.White)
+//            .graphicsLayer { shadowElevation = 10f }
+//    ) {
+//        FloatingActionButton(onClick = {},
+//            modifier = Modifier
+//                .align(Alignment.TopCenter)
+//                .offset(y = (-30).dp), // Posición del FAB sobre el hueco
+//            shape = CircleShape,
+//            contentColor = Color(0xFF7F57FF)){
+//            Icon(Icons.Filled.Add, contentDescription = "Menu")
+//        }
+//        Canvas(modifier = Modifier.matchParentSize()) {
+//            val width = size.width
+//            val height = size.height
+//            val curveSize = 100f
+//            val curveHeight = 140f
+//
+//            val path = Path().apply {
+//                moveTo(0f, 0f)
+//                lineTo((width / 2) - curveSize, 0f)
+//                quadraticBezierTo(width / 2, curveHeight, (width / 2) + curveSize, 0f)
+//                lineTo(width, 0f)
+//                lineTo(width, height)
+//                lineTo(0f, height)
+//                close()
+//            }
+//            drawPath(path, color = Color.White)
+//        }
+//
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(horizontal = 20.dp, vertical = 12.dp),
+//            horizontalArrangement = Arrangement.SpaceBetween,
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            items.forEachIndexed { index, item ->
+//                IconButton(onClick = { selected = index }) {
+//                    Icon(
+//                        imageVector = icons[index],
+//                        contentDescription = item,
+//                        tint = if (selected == index) Color(0xFF7F57FF) else Color.Gray,
+//                        modifier = Modifier.size(28.dp)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
