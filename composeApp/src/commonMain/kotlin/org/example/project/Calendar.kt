c3bb518c24b58b304583f57bf8af24389ac6fd32
Daniel Marin Pacheco <damapa2003@gmail.com>
2025-03-28 10:34:55 +0100

    calendario con grafico, boton flotante y bottom appbarr

--- composeApp/src/commonMain/kotlin/org/example/project/Calendar.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/Calendar.kt
@@ -1,5 +1,6 @@
 package org.example.project
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,13 +10,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +37,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
@@ -129,30 +144,85 @@ fun obtenerPrimerDiaSemana(año: Int, mes: Int): Int {
     return primerDia.ordinal  // Ajuste para empezar en lunes
 }
 
-fun obtenerDiasMes(numMes: Int = 0, numAnio: Int = 0): Int {
-    val fechaActual = diaActual()
-    val mes = fechaActual.plus(DatePeriod(months = numMes)).month
-    val anio = fechaActual.plus(DatePeriod(years = numAnio)).year
-
-    return when (mes) {
-        kotlinx.datetime.Month.FEBRUARY -> if (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0)) 29 else 28
-        kotlinx.datetime.Month.APRIL, kotlinx.datetime.Month.JUNE,
-        kotlinx.datetime.Month.SEPTEMBER, kotlinx.datetime.Month.NOVEMBER -> 30
-
-        else -> 31
-    }
-}
-
-fun diaSemana(dia: Int, mes: Int, anio: Int): DayOfWeek {
-    return LocalDate(anio, mes, dia).dayOfWeek
-}
-
 fun diaActual(): LocalDate {
     return Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
 }
+
 @Composable
 fun PantallaCalendario() {
     MaterialTheme {
-        Calendar()
+        Scaffold(bottomBar = {
+            BottomNavigationBar()
+        }) {
+            Column {
+                Calendar()
+                XYSamplePlot()
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar() {
+    var selected by remember { mutableStateOf(0) }
+    val items = listOf("Home", "Calendar", "Notifications", "Profile")
+    val icons = listOf(
+        Icons.Filled.Home,
+        Icons.Filled.DateRange,
+        Icons.Filled.Notifications,
+        Icons.Filled.Person
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .background(Color.White)
+            .graphicsLayer { shadowElevation = 10f }
+    ) {
+        FloatingActionButton(onClick = {},
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = (-30).dp), // Posición del FAB sobre el hueco
+            shape = CircleShape,
+            contentColor = Color(0xFF7F57FF)){
+            Icon(Icons.Filled.Add, contentDescription = "Menu")
+        }
+        Canvas(modifier = Modifier.matchParentSize()) {
+            val width = size.width
+            val height = size.height
+            val curveSize = 100f
+            val curveHeight = 140f
+
+            val path = Path().apply {
+                moveTo(0f, 0f)
+                lineTo((width / 2) - curveSize, 0f)
+                quadraticBezierTo(width / 2, curveHeight, (width / 2) + curveSize, 0f)
+                lineTo(width, 0f)
+                lineTo(width, height)
+                lineTo(0f, height)
+                close()
+            }
+            drawPath(path, color = Color.White)
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items.forEachIndexed { index, item ->
+                IconButton(onClick = { selected = index }) {
+                    Icon(
+                        imageVector = icons[index],
+                        contentDescription = item,
+                        tint = if (selected == index) Color(0xFF7F57FF) else Color.Gray,
+                        modifier = Modifier.size(28.dp)
+                    )
+                }
+            }
+        }
     }
 }
